f90c3910b9f4feced10a57eb52517ec755789544
as known as 3.14 <happy.535240127@stu.untar.ac.id>
2025-03-31 17:54:52 +0700

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  if(!req.session.user) {
+    res.redirect('/auth/login');
+  }
+  else {
+    res.render('pages/home', {user: req.session.user});
+  }
+});
+
+module.exports = router;

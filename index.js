4b7ea676f493102dcf2666da1de94fe75913cc9f
as known as 3.14 <happy.535240127@stu.untar.ac.id>
2025-03-31 17:36:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// hello
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require('express-session');
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(session({
+  secret: 'some_secret_key',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+const index = require("./routes/index");
+const auth = require("./routes/auth");
+
+app.use('/', index)
+app.use("/auth", auth);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});

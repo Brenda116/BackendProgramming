4da4a09800e53b4d517c757200bcda963af5b031
as known as 3.14 <happy.535240127@stu.untar.ac.id>
2025-03-31 18:46:44 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,32 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/login',m(req, res) => {
-  if(req.session.user) {
-    res.redirect('/');
-  }
-  else {
-    res.render('pages/login');
-  }
+    if(req.session.user) {
+        res.redirect('/');
+    }
+    else {
+        res.render('pages/login');
+    }
 });
 
 router.post("login', async function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
+    const username = req.body.username;
+    const password = req.body.password;
 
-  if(username === "admin" && password === "admin") {
-    req.session.user = "admin";
-    res.redirect("/");
-  }
-  else {
-    return res.render("pages/login", {
-      error: "Wrong username or password"
-    });
-  }
+    if(username === "admin" && password === "admin") {
+        req.session.user = "admin";
+        res.redirect("/");
+    }
+    else {
+        return res.render("pages/login", {
+            error: "Wrong username or password"
+        });
+    }
 });
 
 router.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.redirect('/auth/login');
+    req.session.destroy();
+    res.redirect('/auth/login');
 });
 
 module.exports = router;

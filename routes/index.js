496506ae1b152fbe56c38f26a90339d5d71b69ba
as known as 3.14 <happy.535240127@stu.untar.ac.id>
2025-03-31 18:47:18 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  if(!req.session.user) {
-    res.redirect('/auth/login');
-  }
-  else {
-    res.render('pages/home', {user: req.session.user});
-  }
+    if(!req.session.user) {
+        res.redirect('/auth/login');
+    }
+    else {
+        res.render('pages/home', {user: req.session.user});
+    }
 });
 
 module.exports = router;

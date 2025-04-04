dbb45dd376d9feb1a7116a3752ccf9339c160232
Kathleen Klavinka <kathleen.535240125@stu.untar.ac.id>
2025-04-05 00:00:05 +0700

    Update insertOne.js

--- config/insertOne.js
+++ config/insertOne.js
@@ -3,12 +3,13 @@ const { MongoClient } = require("mongodb");
 const dbURL = "mongodb://localhost:27017/monseu";
 const dbName = "inventory";
 
+// Fungsi untuk memasukkan data
 async function insertData() {
     try {
         const client = await MongoClient.connect(dbURL);
         const db = client.db(dbName);
 
-        // Insert data to the "inventory" collection
+        // Memasukkan data ke "inventory" collection 
         const newData= await db.collection("inventory")
             .insertOne({
                 name: "Indomie Ayam Bawang",
@@ -23,7 +24,7 @@ async function insertData() {
 
             console.log("Data inserted succesfully: ", newData)
 
-        // Mneutup connection setelah operasi
+        // Menutup connection setelah operasi
         await client.close();
     } catch (error) {
         console.error("Error inserting data:", error);

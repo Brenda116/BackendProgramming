512890ca3c438377e3a18a31a8df391d9471860d
Kathleen Klavinka <kathleen.535240125@stu.untar.ac.id>
2025-04-08 15:17:32 +0700

    Update Stock.js

--- models/Stock.js
+++ models/Stock.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const StockSchema = new mongoose.Schema({
     nama_barang: { type: String, required: true },
+    deskripsi: { type: String, required: true },
     jumlah: { type: Number, required: true },
-    harga: { type: Number, required: true }
+    harga: { type: Number, required: true },
+    ketersediaan: { type: Boolean, required: true }
 });
 
 module.exports = mongoose.model("Stock", StockSchema);

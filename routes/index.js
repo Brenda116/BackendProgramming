00a2f350b87fe51975a6ae8550e2843b8c92715b
as known as 3.14 <happy.535240127@stu.untar.ac.id>
2025-04-08 13:21:43 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const Stock = require('../models/Stock'); //Update
+
 router.get('/', (req, res) => {
     if(!req.session.user) {
         res.redirect('/auth/login');
@@ -9,5 +11,15 @@ router.get('/', (req, res) => {
         res.render('pages/home', {user: req.session.user});
     }
 });
+
+router.get('/stocks', async (req, res) => {
+    if(!req.session.user) {
+        res.redirect('/auth/login');
+        return;
+    }
+
+    const stocks = await Stock.find();
+    res.render('pages/stock', { data: stocks });
+}); //Update
 
 module.exports = router;

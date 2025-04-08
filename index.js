ad4c108c1d7a1aa67c27705f99fbd02c0d70180e
as known as 3.14 <happy.535240127@stu.untar.ac.id>
2025-04-08 13:27:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require('express-session');
+const mongoose = require("mongoose"); //Update
+
+mongoose.connect("mongodb://localhost:27017/uts", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("MongoDB Connected");
+    })
+    .catch(err => console.log(err)); //Update
+
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
+
 app.use(session({
   secret: 'some_secret_key',
   resave: false,
@@ -17,6 +26,7 @@ const auth = require("./routes/auth");
 
 app.use('/', index)
 app.use("/auth", auth);
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });

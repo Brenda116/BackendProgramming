f5e35497b0b2b0f2f11bb5e654542611d3d1b806
Kathleen Klavinka <kathleen.535240125@stu.untar.ac.id>
2025-04-14 09:48:39 +0700

    Add files via upload

---
+++ controllers/delete.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/uts");
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+const produk = require("../models/Stock");
+
+// Function DELETE
+async function deleteInventory () {
+    try {
+        // Menghapus produk
+        await produk.deleteOne({ nama_barang: "Indomie Ayam Bawang" });
+
+        // Mencari produk yang dihapus
+        const data = await produk.findOne({ nama_barang: "Indomie Ayam Bawang" });
+        if (data) {
+            console.log("Find One: " + JSON.stringify(data));
+        } else {
+            console.log("Produk tidak ditemukan."); // Jika produk sudah terhapus
+        }
+    } catch (err) {
+        console.log("Error saat menghapus produk:", err);
+    }
+};
+
+deleteInventory(); // Memanggil fungsi
